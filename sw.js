3fc901d4603fdd4f5c0087023c5a2c0b27639985
geomsozzo <geomsozzo@gmail.com>
2026-09-09 15:29:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Studio Sozzo, guardiano della pagina.
    Tiene una copia dell'app sul telefono, cosi' si apre anche senza campo. */
-const NOME = 'sozzo-20260909-1225';
+const NOME = 'sozzo-20260909-1322';
 const ROBA = [
   './',
   './index.html',
